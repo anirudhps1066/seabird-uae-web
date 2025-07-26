918f4b8413bf2c1bb6a18553c8938890ec90a441
anirudhps1066 <anirudhps1066@gmail.com>
2025-07-26 20:44:53 +0530

    added2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,10 +11,10 @@ import logo from "@/assets/logo.png";
 
 const quickLinks = [
   { name: "Home", href: "/" },
-  { name: "About Us", href: "/about" },
-  { name: "Products", href: "/products" },
-  { name: "Services", href: "/services" },
-  { name: "Contact", href: "/contact" },
+  { name: "About Us", href: "/#about" },
+  { name: "Products", href: "/#products" },
+  { name: "Services", href: "/#services" },
+  { name: "Contact", href: "/#contact" },
 ];
 
 const services = [
